441de913975e2208d1e625deb259a12a5b090497
ArnavDogra <148554688+ArnavDogra@users.noreply.github.com>
2025-08-02 12:59:58 +0000

    Add 03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score="33a";
+
+
+console.log(typeof (score)); 
+
+let valueinNumber =Number(score); 
+console.log(typeof valueInNumber);
+
+let isLoggedIn=1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
