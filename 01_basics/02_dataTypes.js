ae9e3cc5ade37957884e1fc0989f612e4235cdb8
ArnavDogra <148554688+ArnavDogra@users.noreply.github.com>
2025-08-02 11:07:33 +0000

    added dataTypes.js from 01_basics folder

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict "; //treat all JS code as newer version
+//alert( 3+3); //we are using node js not browser 
+
+ //number=>2
+ //null=>standalone value
+ //symbol=>unique 
+
+
+ //object 
+ console(typeof null); 
+ 
